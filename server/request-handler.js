b839f55e70eb43aa944eff89968e4486c14fab5a
Yasin Khan <yasinkhan500@gmail.com>
2021-12-04 11:20:37 -0800

    added test cases

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,17 +1,3 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -24,33 +10,12 @@ var body = {
 };
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  // console.log('Response: ', response);
 
-  // The outgoing status.
   var statusCode;
 
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
 
   // headers['Content-Type'] = 'text/plain';
 
@@ -72,8 +37,6 @@ var requestHandler = function(request, response) {
       let stringifiedChunk = chunk.toString();
 
       body.data.push(JSON.parse(stringifiedChunk));
-
-      // response.write(JSON.stringify(body.data));
     })
 
     request.on('end', () => {
@@ -82,10 +45,9 @@ var requestHandler = function(request, response) {
 
   } else if (request.method === 'OPTIONS' && request.url === '/classes/messages') {
 
-    statusCode = 200;
+    statusCode = 204;
     response.writeHead(statusCode, headers);
     response.end();
-    // console.log(response);
 
   } else {
     statusCode = 404;
@@ -93,29 +55,7 @@ var requestHandler = function(request, response) {
     response.end('Wrong Endpoint');
   }
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // response.end('Hello, World!');
-  // response.end(JSON.stringify([{data: 'Hello World!'}]));
-
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 
 module.exports.requestHandler = requestHandler;
--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -58,12 +58,47 @@ describe('server', function() {
     });
   });
 
-  it('Should 404 when asked for a nonexistent endpoint', function(done) {
+  it('should 404 when asked for a nonexistent endpoint', function(done) {
     request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
       expect(response.statusCode).to.equal(404);
       done();
     });
   });
 
+  it('should respond to OPTIONS requests for /classes/messages with a 204 status code', function(done) {
+    var requestParams = {method: 'OPTIONS',
+      uri: 'http://127.0.0.1:3000/classes/messages',
+    };
 
-});
+    request(requestParams, function(error, response, body) {
+      expect(response.statusCode).to.equal(204);
+      done();
+    });
+  });
+
+  it('should respond with correct access-control-allow-origin header', function(done) {
+    var defaultCorsHeaders = {
+      'access-control-allow-origin': '*',
+      'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'access-control-allow-headers': 'content-type, accept, authorization',
+      'access-control-max-age': 10,
+    };
+    request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+      expect(response.headers['access-control-allow-origin']).to.deep.equal('*');
+      done();
+    })
+  });
+
+    it('should respond with correct access-control-allow-methods header', function(done) {
+    var defaultCorsHeaders = {
+      'access-control-allow-origin': '*',
+      'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'access-control-allow-headers': 'content-type, accept, authorization',
+      'access-control-max-age': 10,
+    };
+    request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+      expect(response.headers['access-control-allow-methods']).to.deep.equal('GET, POST, PUT, DELETE, OPTIONS');
+      done();
+    })
+  });
+})
